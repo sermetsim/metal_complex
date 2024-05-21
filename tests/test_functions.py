98df24e25135997245cff6e9cdbf550d4e9d1a21
sermetsim <simon.sermet-magdelain@epfl.ch>
2024-05-21 18:16:47 +0200

    Change
    from main_function import *
    to
    from main_functions import *

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,7 +2,7 @@ import pytest
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
-from main_function import *
+from main_functions import *
 
 
 @pytest.mark.parametrize(
