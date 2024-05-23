70b36ef0889456668a33a0f1f0fdca983d4b487a
Simon.Sermet <160873084+sermetsim@users.noreply.github.com>
2024-05-23 16:37:51 +0200

    Update test_functions.py

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,7 +2,7 @@ import pytest
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src/metal_complex")))
-from main_functions import *
+from metal_functions import *
 
 
 @pytest.mark.parametrize(
