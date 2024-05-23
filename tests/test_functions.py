ebc4be844240259e4d5573a9d647eba7ffd695c6
sermetsim <simon.sermet-magdelain@epfl.ch>
2024-05-23 16:32:51 +0200

    Changed directory

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src/metal_complex")))
 from main_functions import *
 
 
